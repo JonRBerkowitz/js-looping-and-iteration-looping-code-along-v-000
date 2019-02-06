dcfc128f87dd3440d4a373d6fc79bc99473ab269
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-06 23:34:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function printBadges(ary) {
+  for (let i = 0; i < ary.length; i++) {
+    console.log(`Welcome ${ary[i]}! You are employee #${i + 1}.`);
+  }
+  return ary;
+}
